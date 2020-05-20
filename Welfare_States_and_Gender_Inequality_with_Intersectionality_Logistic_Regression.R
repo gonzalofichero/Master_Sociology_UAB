3598fcb1e9d5a887854e02438423e50bb93257a7
gonzalofichero <gonzalo.fce@gmail.com>
2020-05-20 18:37:43 +0200

    Add files via upload

--- Welfare_States_and_Gender_Inequality_with_Intersectionality_Logistic_Regression.R
+++ Welfare_States_and_Gender_Inequality_with_Intersectionality_Logistic_Regression.R
@@ -1,3 +1,31 @@
+#########################################################################################
+#	MIT License
+#
+#	Copyright (c) [2020] [Gonzalo Daniel Garcia: https://github.com/gonzalofichero/]
+#
+#	Permission is hereby granted, free of charge, to any person obtaining a copy
+#	of this software and associated documentation files (the "Software"), to deal
+#	in the Software without restriction, including without limitation the rights
+#	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#	copies of the Software, and to permit persons to whom the Software is
+#	furnished to do so, subject to the following conditions:
+#
+#	The above copyright notice and this permission notice shall be included in all
+#	copies or substantial portions of the Software.
+#
+#	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#	SOFTWARE.
+##########################################################################################
+
+# Title of code: "Welfare_States_and_Gender_Inequality_with_Intersectionality_Logistic_Regression.R"
+# DOI: https://zenodo.org/badge/latestdoi/265623238
+
+
 # Loading packages
 library(tidyverse)
 library(ggplot2)
